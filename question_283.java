dfefe5f14dfeb6e63bdc256b6cc3a05ca4aec2a5
Cccccyf <1902383836@qq.com>
2022-01-30 15:46:58 +0800

    我是shabi

---
+++ question_283.java
@@ -0,0 +1,29 @@
+package leetcode;
+//leetcode_283移动零
+public class question_283 {
+    public void moveZeroes(int[] nums) {
+        if(nums.length == 1) return;
+        int i = 0;
+        int j = i;
+        while(i<nums.length&&j<nums.length)
+        {
+            while(i< nums.length&&nums[i]!=0){
+                i++;
+            }
+            if(i<nums.length) {
+                while (j <= i || (nums[j] == 0&&j<nums.length)) {
+                    j++;
+                }
+                if (j < nums.length && nums[j] != 0) {
+                    swap(nums, i, j);
+                }
+                j++;
+            }
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
