7d530738e01f44ef6de43d628463804a933b7c7e
Cccccyf <1902383836@qq.com>
2022-02-11 20:28:43 +0800

    二叉树的前序遍历

---
+++ quesiton_144.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode_144二叉树的前序遍历
+public class quesiton_144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preTraversal(root,res);
+        return  res;
+    }
+    public void  preTraversal(TreeNode root,List<Integer> list){
+        if(root == null) return;
+        list.add(root.val);
+        preTraversal(root.left,list);
+        preTraversal(root.right,list);
+    }
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+}
