31594334f33412788e0208cbfddbaf0c32bbac84
Cccccyf <1902383836@qq.com>
2022-02-26 21:40:42 +0800

    用队列实现栈

---
+++ question_20.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Stack;
+
+//leetcode_20有效的括号
+public class question_20 {
+    public boolean isValid(String s) {
+        Stack<Character> brackets = new Stack<>();
+        for(int i = 0;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(ch == '('){
+                brackets.push(')');
+            }else if(ch == '['){
+            brackets.push(']');
+            }else if(ch == '{'){
+                brackets.push('}');
+            }else if(brackets.empty()||brackets.peek()!=ch){
+                return false;
+            }else{
+                brackets.pop();
+            }
+        }
+        return brackets.empty();
+    }
+}
